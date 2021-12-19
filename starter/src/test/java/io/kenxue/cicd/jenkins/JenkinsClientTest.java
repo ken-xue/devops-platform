8b02ef70f29cc1dd5155c7993631b9e66d1a3951
mikey <mikey@91zhen.com>
2021-12-19 17:19:28 +0800

    feature:add jenkins dependencies

---
+++ starter/src/test/java/io/kenxue/cicd/jenkins/JenkinsClientTest.java
@@ -0,0 +1,15 @@
+package io.kenxue.cicd.jenkins;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JenkinsClientTest {
+
+    @Test
+    private void test(){
+//        JenkinsServer jenkins = new JenkinsServer(new URI("http://localhost:8080/jenkins"), "admin", "password")
+//        Map<String, Job> jobs = jenkins.getJobs();
+//        JobWithDetails job = jobs.get("My Job").details();
+    }
+}
